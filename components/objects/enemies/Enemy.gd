09e9f67d15fdd8e750d7c9d705fc156bac2a834d
wizardkingrabbit <jingtil2@uci.edu>
2020-01-29 12:26:14 +0800

    Revert "Merge branch 'master' of https://github.com/blongty/kingcastlehigh"
    This reverts commit f1a3c5b2f9654a61b0f6a1d45b95f5eee9da6d3a, reversing
    changes made to ba90974cb94946b5a9832514dc0917aa97efcb51.

--- components/objects/enemies/Enemy.gd
+++ components/objects/enemies/Enemy.gd
@@ -6,21 +6,6 @@ class_name Enemy
 func _ready():
 	($Anim as AnimationPlayer).play("Flicker")
 
-var speed = 50
-var path : = PoolVector2Array()
-
-func _process(delta):
-	if path.size() > 0:
-		_walk(delta)
-	
-func _walk(delta):
-	var distance_to_walk = speed * delta
-	while distance_to_walk > 0 and path.size() > 0:
-		var distance_to_next_point = position.distance_to(path[0])
-		if distance_to_walk <= distance_to_next_point:
-			position += position.direction_to(path[0]) * distance_to_walk
-		else:
-			position = path[0]
-			path.remove(0)
-		distance_to_walk-=distance_to_next_point 
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
