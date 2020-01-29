f50beb9ba4b41ee94fdf0bf682b36359c30658e5
usaradark <gamekid263@gmail.com>
2020-01-29 15:33:35 -0800

    Enemies     - Enemies now have a life     - Enemies are destroyed upon reaching 0 life     - By default, instanced enemies have 1 life
    snowball_demo
        - One enemy is now bigger and has more life to demo life

--- components/objects/enemies/Enemy.gd
+++ components/objects/enemies/Enemy.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 class_name Enemy
 
+export(int) var life = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	($Anim as AnimationPlayer).play("Flicker")
@@ -12,4 +14,6 @@ func _ready():
 
 
 func _on_snowball_enter(area : Area2D):
-	queue_free()
+	life -= 1
+	if life <= 0:
+		queue_free()
